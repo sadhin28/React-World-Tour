0711ee89a344451771b0f9e9a5d53f67106e79e6
sadhin28 <taosifsadhin113@gmail.com>
2025-04-29 20:29:26 +0600

    add country name

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -13,7 +13,8 @@ const Countries = () => {
             <h3>Countries : {countries.length} </h3>
          
         {
-            countries.map(country=><Country country={country}></Country>)
+            countries.map(country=><Country 
+                key={country.cca3} country={country}></Country>)
         }
 
         </div>
--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,7 +1,8 @@
+import './Country.css'
 const Country = ({country}) => {
     const {name}=country
     return (
-        <div>
+        <div className='country'>
             <h4>Country Name : {name?.common} </h4>
         </div>
     );
