c3a2e5fa291397d241979a700c3a65a072039abb
sadhin28 <taosifsadhin113@gmail.com>
2025-04-29 22:14:12 +0600

    add population

--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -2,11 +2,13 @@ import './Country.css';
 const Country = ({country}) => {
     console.log(country)
 
-    const {name,flags}=country
+    const {name,flags,population,area}=country
     return (
         <div className='country'>
             <h4>Country Name : {name?.common} </h4>
             <h5>Official Name : {name?.official}</h5>
+            <h5>Population : {population}</h5>
+            <h6>Area : {area}</h6>
             <img className='flags' src={flags?.png} alt="" />
         </div>
     );
