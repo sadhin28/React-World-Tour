0241972bc7b5081d0cd5fb92352232af9bde7096
sadhin28 <taosifsadhin113@gmail.com>
2025-04-29 21:51:56 +0600

    add flag

--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,10 +1,13 @@
-import './Country.css'
+import './Country.css';
 const Country = ({country}) => {
-    const {name}=country
+    console.log(country)
+
+    const {name,flags}=country
     return (
         <div className='country'>
             <h4>Country Name : {name?.common} </h4>
             <h5>Official Name : {name.official}</h5>
+            <img className='flags' src={flags.png} alt="" />
         </div>
     );
 };
